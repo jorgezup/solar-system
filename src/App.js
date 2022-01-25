af07ad2695d5934173262b522c5c1459071dafaf
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-25 14:42:12 -0400

    Requisito 06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import PlanetCard from './components/PlanetCard';
+
+import planets from './data/planets';
 
 class App extends React.Component {
   render() {
@@ -8,6 +12,13 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        {planets.map((planet, index) => (
+          <PlanetCard
+            key={index}
+            planetName={planet.name}
+            planetImage={planet.image}
+          />
+        ))}
       </>
     );
   }
